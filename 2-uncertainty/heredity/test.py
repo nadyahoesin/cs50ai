c3afaa0fcbd62cadd7e65cc9fb76ea30125f7d6a
Nadya Hoesin <nadyahoesin@MacBook-Air.local>
2021-01-03 03:45:09 +0700

    idk, dumb changes

--- 2-uncertainty/heredity/test.py
+++
@@ -1,9 +0,0 @@
-import heredity
-
-people = {
-  'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': None},
-  'James': {'name': 'James', 'mother': None, 'father': None, 'trait': True},
-  'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
-}
-
-print(heredity.joint_probability(people, {"Harry"}, {"James"}, {"James"}))
